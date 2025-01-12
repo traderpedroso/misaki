e015f1fae31bcea264b70b2f00d388857275f1f2
hexgrad <hello@hexgrad.com>
2025-01-12 15:50:45 -0800

    Add __init__.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='misaki',
-    version='0.2.1',
+    version='0.2.2',
     packages=find_packages(),
     install_requires=[
         'num2words',
