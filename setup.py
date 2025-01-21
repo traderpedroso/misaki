820f2a1872fbb5279e25684832a792e62098a50d
hexgrad <hello@hexgrad.com>
2025-01-20 22:11:33 -0800

    Fix heard

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='misaki',
-    version='0.4.7',
+    version='0.4.8',
     packages=find_packages(),
     package_data={
         'misaki': ['data/*.json', 'data/*.txt', 'hangul/data/*.csv', 'hangul/data/*.tsv'],
