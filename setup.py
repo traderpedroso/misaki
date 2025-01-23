f8cc84aba996bc77c0f9cdaed26e4f1068637837
hexgrad <hello@hexgrad.com>
2025-01-22 16:45:40 -0800

    Fix stem_s

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='misaki',
-    version='0.5.4',
+    version='0.5.5',
     packages=find_packages(),
     package_data={
         'misaki': ['data/*.json', 'data/*.txt', 'hangul/data/*.csv', 'hangul/data/*.tsv'],
